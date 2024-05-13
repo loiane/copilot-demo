6bf7c29bac55c183ab6726c9c8436074bfb14016
Loiane <loianeg@gmail.com>
2024-05-13 19:12:38 -0400

    first commit

---
+++ src/main/java/com/loiane/copilotdemo/CopilotDemoApplication.java
@@ -0,0 +1,13 @@
+package com.loiane.copilotdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopilotDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopilotDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loiane/copilotdemo/CopilotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loiane.copilotdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopilotDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
