7206fa1494305aa073f97ff5ef79150cbf661579
Loiane Groner <loianeg@gmail.com>
2024-05-13 21:50:14 -0400

    Replace javax with jakarta in imports

--- src/main/java/com/loiane/copilotdemo/model/Product.java
+++ src/main/java/com/loiane/copilotdemo/model/Product.java
@@ -1,13 +1,13 @@
 package com.loiane.copilotdemo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @Entity
